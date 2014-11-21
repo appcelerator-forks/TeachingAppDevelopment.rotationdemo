6654f97fff8b22fc7b77fd2949f08993724666e4
Claus Stilborg <cb@nebulators.com>
2014-11-21 11:05:53 +0100

    Played with it some more

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,14 @@
 var angle = 0;
 function doClickRotateImage(e){
 	// rotate the image
-	angle += 20;
-	if(angle >= 360){
+	angle += 45;
+	if(angle >= 180){
 		angle = 0;
 	}
 	var m = Ti.UI.create2DMatrix({ 
-    	rotate: angle 
+    	rotate: angle,
+    	scale: 1.5
+    	 
 	});
 	var a = {
   		transform: m,
