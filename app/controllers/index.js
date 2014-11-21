f644a442ee566447bbe8af92084cc89579841498
Claus Stilborg <cb@nebulators.com>
2014-11-21 10:53:43 +0100

    Rotation and transformation added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,20 @@
+var angle = 0;
+function doClickRotateImage(e){
+	// rotate the image
+	angle += 20;
+	if(angle >= 360){
+		angle = 0;
+	}
+	var m = Ti.UI.create2DMatrix({ 
+    	rotate: angle 
+	});
+	var a = {
+  		transform: m,
+  		duration: 2000,
+  		autoreverse: true
+	};
+	
+	$.ivRotateImage.animate(a);
+}
+
 $.index.open();
